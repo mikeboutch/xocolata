2ced1fcd65a2b4dc11bd5b11e9fa8d1d9637f5c5
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2019-03-18 13:29:41 -0400

    Add xocolata-dev for developing propose of the configuration mech...

---
+++ xocolata-dev/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$nuspecDir = "$(Split-Path -parent $toolsDir)"
+Write-Host "tooldir:$toolsDir"
+Write-Host "PSScriptRoot:$PSScriptRoot"
+Write-Host "packageFolder:$packageFolder"
+Write-Host "env.packageFolder:$env:packageFolder"
+Write-Host ""
+Write-Host "nuspecDir:$nuspecDir"
+
+$pp = Get-PackageParameters
+Write-Output "pp:$($pp|Format-Table|Out-String)"
+
+
+$packageConfig=Get-Content (Join-Path $toolsDir "packageConfig.json") | ConvertFrom-Json
+Write-Output "packageConfig:$($packageConfig.envVars|Format-List|Out-String)"
+
+
+
+Start-Sleep 100
+generate a error
